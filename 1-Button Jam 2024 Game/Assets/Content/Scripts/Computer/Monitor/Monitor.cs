8691f78a068d7a54d98d448ac714eade11ef46c0
OzoneY1911 <121692316+OzoneY1911@users.noreply.github.com>
2024-11-25 15:04:13 +0100

    Updated assets

--- 1-Button Jam 2024 Game/Assets/Content/Scripts/Computer/Monitor/Monitor.cs
+++ 1-Button Jam 2024 Game/Assets/Content/Scripts/Computer/Monitor/Monitor.cs
@@ -1,31 +1,34 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class Monitor : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer _outlineRenderer;
+    [SerializeField] private MeshRenderer _indicatorRenderer;
     [SerializeField] private GameObject _popUpObject;
 
     public GameObject PopUpObject => _popUpObject;
     public PopUp PopUpScript => _popUpObject.GetComponent<PopUp>();
     public bool HasPopUp => _popUpObject.activeSelf;
 
-    private MaterialPropertyBlock _enableOutlinePropertyBlock;
+    private MaterialPropertyBlock _colorPropertyBlock;
 
     private void Awake()
     {
-        _enableOutlinePropertyBlock = new MaterialPropertyBlock();
+        _colorPropertyBlock = new MaterialPropertyBlock();
     }
 
     public void Select()
     {
-        _enableOutlinePropertyBlock.SetFloat("_Enable_Outline", 1f);
-        _outlineRenderer.SetPropertyBlock(_enableOutlinePropertyBlock, 1);
+        _colorPropertyBlock.SetColor("_BaseColor", Color.red);
+        _colorPropertyBlock.SetColor("_EmissionColor", Color.red);
+        _indicatorRenderer.SetPropertyBlock(_colorPropertyBlock, 1);
     }
 
     public void Deselect()
     {
-        _enableOutlinePropertyBlock.SetFloat("_Enable_Outline", 0f);
-        _outlineRenderer.SetPropertyBlock(_enableOutlinePropertyBlock, 1);
+        _colorPropertyBlock.SetColor("_BaseColor", new Color(170f, 255f, 0f));
+        _colorPropertyBlock.SetColor("_EmissionColor", new Color(170f, 255f, 0f));
+        _indicatorRenderer.SetPropertyBlock(_colorPropertyBlock, 1);
     }
 
     public void Click()
